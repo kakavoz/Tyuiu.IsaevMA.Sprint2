42ab3a47adf7efd6c2428a0c7f824a71580dcb14
maxim isaev <Microsoft10max@gmail.com>
2023-10-18 19:04:33 +0500

    task2

---
+++ tyuiu.isaevma.sprint2.task2.v17.lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace tyuiu.isaevma.sprint2.task2.v17.lib
+{
+    public class DataService : ISprint2Task2V17
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+
+            if (((x >= 3) && (x <= 5) && (y >= 3) && (y <= 7) || (y == 11)) || ((x == 6) && (y <=11) && (y >= 5)) || ((x == 7) && (y <= 12) && (y >= 5)) || ((x == 8) && (y >= 5) && (y <= 7))|| ((x == 9) && (y >= 3) && (y <= 7)) || (x == 10) && (y >= 3) && (y <= 12) || ((x >= 11) && (x <= 12) && (y >= 3) && (y <= 5) || (y >= 10) && (y <= 12)) || (x == 13) && (y ==4 ))
+
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ tyuiu.isaevma.sprint2.task2.v17/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.isaevma.sprint2.task2.v17.lib;
+
+namespace tyuiu.isaevma.sprint2.task2.v17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Исаев.М.А. | ACОиУБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                                          *");
+            Console.WriteLine("* Тема: Алгоритмы разветвляющейся структуры                                                                          *");
+            Console.WriteLine("* Задание #2                                                                                                         *");
+            Console.WriteLine("* Вариант #17                                                                                                        *");
+            Console.WriteLine("* Выполнил: Иcаев Максим Александрович | ACOиУБ-23-3                                                                 *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                           *");
+            Console.WriteLine("* Написать программу на, которая запрашивает целые значения с клавиатуры                                             *");
+            Console.WriteLine("* и вычисляет находится ли точка с координатами X,Y в заштрихованной области.                                        *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                   *");
+            Console.WriteLine("**********************************************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            DataService ds = new DataService();
+            bool res = ds.CheckDotInShadedArea(x, y);
+
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                         *");
+            Console.WriteLine("**********************************************************************************************************************");
+            if (res)
+            {
+                Console.WriteLine("Точка находиться в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка не находиться в заштрихованной области");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
