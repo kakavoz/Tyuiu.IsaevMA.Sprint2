4ea1581912b8fc4f76a1821d28dc2d68f759c520
kakavoz <144474774+kakavoz@users.noreply.github.com>
2023-10-19 20:55:32 +0500

    Add files via upload

---
+++ Tyuiu.IsaevMA.Sprint2.Task4.V19.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.IsaevMA.Sprint2.Task4.V19.Lib;
+
+namespace Tyuiu.IsaevMA.Sprint2.Task4.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.IsaevMA.Sprint2.Task4.V19/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.IsaevMA.Sprint2.Task4.V19.Lib;
+
+namespace Tyuiu.IsaevMA.Sprint2.Task4.V19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #2 | Выполнил: Исаев.М.А. | ACОиУБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                                          *");
+            Console.WriteLine("* Тема: Тернарный оператор                                                                                           *");
+            Console.WriteLine("* Задание #4                                                                                                         *");
+            Console.WriteLine("* Вариант #19                                                                                                        *");
+            Console.WriteLine("* Выполнил: Исаев Максим Александрович | ACOиУБ-23-3                                                                 *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                           *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение с использованием тернарного оператора                     *");
+            Console.WriteLine("* где пользователь вводит значение переменной X,Y с клавиатуры.                                                      *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                   *");
+            Console.WriteLine("**********************************************************************************************************************");
+
+            Console.WriteLine("Введите значение переменной X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение переменной Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                         *");
+            Console.WriteLine("**********************************************************************************************************************");
+
+            Console.WriteLine("Значение функции = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
