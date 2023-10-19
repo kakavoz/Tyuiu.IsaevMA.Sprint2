9fcaca7a541fb771c0415c2445f9efd8e0e9ff33
kakavoz <144474774+kakavoz@users.noreply.github.com>
2023-10-19 19:24:19 +0500

    Add files via upload

---
+++ Tyuiu.IsaevMA.Sprint2.Task0.V23.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace tyuiu.isaevma.sprint2.v23.lib
+{
+    public class DataService : ISprint2Task0V23
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = x == y - 690;
+            res[1] = x != y - 690;
+            res[2] = x < y;
+            res[3] = x > y - 691;
+            res[4] = x <= y - 691;
+            res[5] = x + 690 >= y;
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.IsaevMA.Sprint2.Task0.V23/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.isaevma.sprint2.v23.lib;
+
+namespace tyuiu.isaevma.sprint2.v23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            int x = 105;
+            int y = 795;
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.Title = "Спринт #2 | Выполнил: Исаев.М.А | ACОиУБ-23-3";
+            //Длина строки 75 символов
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* Спринт #2                                                                                                          *");
+            Console.WriteLine("* Тема: Операции сравнения                                                                                           *");
+            Console.WriteLine("* Задание #0                                                                                                         *");
+            Console.WriteLine("* Вариант #23                                                                                                        *");
+            Console.WriteLine("* Выполнил: Исаев Максим Александрович | ACOиУБ-23-3                                                                 *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                                                           *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=, последовательность операций не должна нарушаться)  *");
+            Console.WriteLine("* и арифметических выражений, которая вернет логическую последовательность(массив):                                  *");
+            Console.WriteLine("* (True, False, True, True, False,True), при x = 105, y = 795                                                    *");
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                                                   *");
+            Console.WriteLine("**********************************************************************************************************************");
+
+            Console.WriteLine("X = " + x);
+            Console.WriteLine("Y = " + y);
+
+            Console.WriteLine("**********************************************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                                                         *");
+            Console.WriteLine("**********************************************************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
